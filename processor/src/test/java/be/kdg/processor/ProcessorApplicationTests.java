339a11ce9ef588b64104277fac9ea5bc9fed6772
Glenn Geysen <glenn.geysen@student.kdg.be>
2018-10-05 08:55:12 +0200

    verzenden en ontvangen van messages

--- processor/src/test/java/be/kdg/processor/ProcessorApplicationTests.java
+++ processor/src/test/java/be/kdg/processor/ProcessorApplicationTests.java
@@ -1,16 +1,27 @@
 package be.kdg.processor;
 
+import be.kdg.sa.services.CameraServiceProxy;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ProcessorApplicationTests {
 
-    @Test
-    public void contextLoads() {
-    }
+    private CameraServiceProxy cameraServiceProxy = new CameraServiceProxy();
 
+    @Test
+    public void testCameraService() {
+        try {
+            String cameraInfo = cameraServiceProxy.get(1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
