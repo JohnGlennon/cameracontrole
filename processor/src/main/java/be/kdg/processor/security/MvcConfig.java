a13d3ddc39b4ef033861e8eb3a8ce5e8ca71954a
Glenn Geysen <glenn.geysen@student.kdg.be>
2018-10-31 16:37:52 +0100

    aanloggen via een vast adminaccount

--- processor/src/main/java/be/kdg/processor/security/MvcConfig.java
+++ processor/src/main/java/be/kdg/processor/security/MvcConfig.java
@@ -1,21 +1,18 @@
-package be.kdg.processor.fine;
+package be.kdg.processor.security;
 
-import org.modelmapper.ModelMapper;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class MvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
         registry.addViewController("/").setViewName("home");
-    }
-
-    @Bean
-    public ModelMapper modelMapper() {
-        return new ModelMapper();
+        registry.addViewController("/summary").setViewName("summary");
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/factors").setViewName("factors");
     }
 }
